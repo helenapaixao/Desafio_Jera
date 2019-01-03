2371af08a40a611a6317a25b07e177bb4b566f99
Helena Paixão <hp.helenapaixao@gmail.com>
2019-01-03 19:08:21 -0300

    Update pomodoro.js
    acrescentei as funções de começar o contador o pomodoro, intervalo e o de parar o cronometro

--- Pomodoro/pomodoro.js
+++ Pomodoro/pomodoro.js
@@ -1,55 +1,91 @@
 
+var pomodoro = {
 
-var pomodoro ={
-
-
-    comecar : false,
-    minutos : 0 ,
+    comecar: false,
+    minutos: 0,
     segundos: 0,
-    animacaoAltura:0,
-    animacaoLargura: 0,
+    animaAltura: 0,
+    animaIncremento: 0,
     intervalo: null,
-    minutosNoDom: null,
-    segundosNoDom: null,
-    animacaoDom: null,
-    init: function(){
+    minutosDom: null,
+    segundosDom: null,
+    animaDom: null,
 
+    iniciar() {
         var self = this;
+        this.minutosDom = document.getElementById("minutos");
+        this.segundosDom =document.getElementById("segundos");
+        this.animaDom = document.getElementById("card");
 
-        this.minutosNoDom= document.querySelector('#minutos');
-        this.segundosNoDom = document.querySelector('#segundos');
-        this.animacaoDom = document.querySelector('#animacaopreencher');
-
-        this.intervalo = setInterval(function(){
-            self.intervaloCallback.aply(self);
+        this.intervalo = setInterval(function () {
+            self.intervalCallback.apply(self);
         }, 1000);
-
-        document.querySelector('#comecar').onclick = function(){
-            self.startComecar.aply(self);
+        document.querySelector('#comecar').onclick = function () {
+            self.comecarPomodoro.apply(self);
         };
-        document.querySelector('#intervalo').onclick = function(){
-            self.intervalo.aply(self);
-        };
-
-        document.querySelector('#parar').onclick = function(){
-            self.parar.aply(self);
+        document.querySelector('#intervalo').onclick = function () {
+            self.comecarIntervalo.apply(self);
         };
 
-
+        document.querySelector('#parar').onclick = function () {
+            self.pararPomodoro.apply(self);
+        };
     },
-
-    deletarVariaveis : function(mins,sec,comecar){
+    limparVariaveis(mins, segs, comecar) {
         this.minutos = mins;
-        this.segundos = sec;
+        this.segundos = segs;
         this.comecar = comecar;
-        this.animacaoLargura = 200(this.minutos*60);
-        this.animacaoAltura=0;
+        this.animaIncremento = 200 / (this.minutos * 60);
+        this.animaAltura = 0;
+    },
+    comecarPomodoro() {
+        this.limparVariaveis(25, 0, true);
+    },
+    comecarIntervalo() {
+        this.limparVariaveis(5, 0, true);
     },
 
-    startComecar: function(){
-        this.deletarVariaveis(25,0,true);
+    pararPomodoro() {
+        this.limparVariaveis(25, 0, false);
+        this.atualizar();
     },
-    
 
+    verificarNumero(numero) {
+        if (numero < 10) {
+            return "0" + parseInt(numero, 10);
+        }
+        return numero;
+    },
 
-}
+    atualizar() {
+
+        this.minutosDom.innerHTML = this.verificarNumero(this.minutos);
+        this.segundosDom.innerHTML = this.verificarNumero(this.segundos);
+        this.animaAltura = this.animaAltura + this.animaIncremento;
+        this.animaDom.style.height = this.animaAltura + 'px';
+    },
+
+    intervalCallback() {
+        
+        if (!this.comecar) return false;
+        if (this.segundos == 0) {
+            if (this.minutos == 0) {
+                this.tempoCompleto();
+                return;
+            }
+            this.segundos = 59;
+            this.minutos--;
+        } else {
+            this.segundos--;
+        }
+        this.atualizar();
+    },
+    tempoCompleto() {
+        this.comecar = false;
+        this.animaAltura = 0;
+        //acrescentar som 
+    }
+};
+window.onload = function () {
+    pomodoro.iniciar();
+};
