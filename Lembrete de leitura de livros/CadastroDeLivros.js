ed8a6b699296ba3ee91cc01ef0ab9028ad882a64
Helena Paixão <hp.helenapaixao@gmail.com>
2019-01-03 23:50:30 -0300

    Update CadastroDeLivros.js
    add as variáveis ao construtor, criando um objeto chamado livro, array com o nome de livros aonde serão acrescentados os livros, uma flag idMarcado pra saber se é o livro escolhido para o lembrete

--- Lembrete de leitura de livros/CadastroDeLivros.js
+++ Lembrete de leitura de livros/CadastroDeLivros.js
@@ -2,50 +2,59 @@ class CadastroDeLivros {
 
     constructor() {
 
-
+        this.livros = [];
+         this.livro = {};
+        this.contador = 0;
+        this.IdMarcado = null;
 
     }
 
     lerDados() {
+        
+        let nome = document.getElementById("nome").value;
+        let pagina = document.getElementById("pagina").value;
 
+        this.livro = {};
 
+        this.livro.nome = nome;
+        this.livro.pagina = pagina;
+        this.livro.lembrete = false;
     }
     salvar() {
-
+     
     }
     validar() {
-
+      
     }
     limpar() {
-
 
     }
 
     marcar(id) {
 
-
     }
-
 
 
     cadastrarLembrete(id) {
 
 
+      
     }
 
     excluirLembrete(id) {
 
 
-
-
-
     }
     excluir(id) {
-
-
     }
+
     criarTabela() {
 
 
+        }
 
     }
+
+}
+
+let biblioteca = new CadastroDeLivros();
